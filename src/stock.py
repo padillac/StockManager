59aff2898dc8f347127e92d5c2622838fcebf993
padillac <shinimaninima@gmail.com>
2020-12-18 21:05:24 -0500

    Updating stock class

--- src/stock.py
+++ src/stock.py
@@ -8,10 +8,28 @@ class Stock:
         self.stock_url = "https://finance.yahoo.com/quote/{0}?p={0}".format(ticker)
         self.financial_url = "https://finance.yahoo.com/quote/{0}/financials?p={0}".format(ticker)
 
+        self.updateData()
+
+
+
+
+    def updateData(self):
         self._get_raw_data()
+        self._process_data()
+
 
 
     def _get_raw_data(self):
+        '''Download raw data from Yahoo Finance'''
         driver = webdriver.Chrome()
         driver.get(self.stock_url)
+        # SCRAPE STOCK DATA
+        driver.get(self.financial_url)
+        # SCRAPE FINANCE DATA
+
+
+    def _process_data(self):
+        '''Process raw data to create meaningful up-to-date data points'''
+        pass
         
+    
